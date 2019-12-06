cc412906d0b36d79b1db8d4514a1e378160ea89b
Trevor Brindle <tabrindle@gmail.com>
2019-12-06 12:36:52 -0500

    fix: filter for warnings on file level comments

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
-const fetch = require('node-fetch');
-const path = require('path');
+const fetch = require("node-fetch");
+const path = require("path");
 
-const eslintDocsBaseUrl = 'https://github.com/eslint/eslint/blob/master/docs/rules';
+const eslintDocsBaseUrl =
+  "https://github.com/eslint/eslint/blob/master/docs/rules";
 
 const encodeAuthorization = ({ user, password }) => {
-  return `Basic ${new Buffer(`${user}:${password}`, 'binary').toString('base64')}=`;
+  return `Basic ${new Buffer(`${user}:${password}`, "binary").toString(
+    "base64"
+  )}=`;
 };
 
 const requiredParams = (params = {}) => {
   const missing = Object.keys(params)
     .filter(filterKey => params[filterKey] === undefined) // eslint-disable-line no-undefined
-    .reduce((acc, reduceKey) => `${acc} ${reduceKey}`, '');
+    .reduce((acc, reduceKey) => `${acc} ${reduceKey}`, "");
   console.log(missing);
   if (missing) {
-    throw new TypeError(`The following required parameters are missing:${missing}`);
+    throw new TypeError(
+      `The following required parameters are missing:${missing}`
+    );
   } else {
     return true;
   }
@@ -21,44 +26,44 @@ const requiredParams = (params = {}) => {
 
 const commentTopLevelFn = function(lintResults, url, bitbucketUrl, options) {
   const errors = lintResults.reduce((sum, file) => sum + file.errorCount, 0);
-  const errorPlural = errors === 1 ? 'error' : 'errors';
+  const errorPlural = errors === 1 ? "error" : "errors";
 
   if (options.debug) {
-    console.log('POST to: ', url);
+    console.log("POST to: ", url);
     console.log(`[eslint] This PR contains ${errors} lint ${errorPlural}`);
     return;
   }
 
   return fetch(url, {
-    method: 'POST',
+    method: "POST",
     body: JSON.stringify({
-      text: `[eslint] This PR contains ${errors} lint ${errorPlural}`,
+      text: `[eslint] This PR contains ${errors} lint ${errorPlural}`
     }),
-    credentials: 'include',
+    credentials: "include",
     headers: {
       Authorization: encodeAuthorization(options),
-      'Content-Type': 'application/json',
-    },
+      "Content-Type": "application/json"
+    }
   })
     .then(r => {
       if (r.ok) return r.json();
     })
     .then(response => {
-      if(options.createTask)
+      if (options.createTask)
         return fetch(`${bitbucketUrl}/rest/api/1.0/tasks`, {
-          method: 'POST',
+          method: "POST",
           body: JSON.stringify({
             text: `fix ${errors} lint ${errorPlural}`,
             anchor: {
               id: response.id,
-              type: 'COMMENT',
-            },
+              type: "COMMENT"
+            }
           }),
-          credentials: 'include',
+          credentials: "include",
           headers: {
             Authorization: encodeAuthorization(options),
-            'Content-Type': 'application/json',
-          },
+            "Content-Type": "application/json"
+          }
         }).then(r => {
           if (r.ok) return r.json();
         });
@@ -71,73 +76,67 @@ const commentFileLevelFn = function(lintResults, url, options) {
     if (file.messages.length) {
       return file.messages
         .filter(message => {
-          if (!options.warnings && message.severity === 1) return false;
+          return !(!options.warnings && message.severity === 1);
         })
         .map(message => {
           if (options.debug) {
-            console.log('POST to: ', url);
+            console.log("POST to: ", url);
             console.log(
-              `[eslint] ${message.message} - ([${message.ruleId}](${eslintDocsBaseUrl}/${
-                message.ruleId
-              }.md))`,
+              `[eslint] ${message.message} - ([${message.ruleId}](${eslintDocsBaseUrl}/${message.ruleId}.md))`
             );
             console.log({
               line: message.line,
-              lineType: 'ADDED',
-              fileType: 'TO',
-              path: file.filePath.split(process.cwd())[1],
+              lineType: "ADDED",
+              fileType: "TO",
+              path: file.filePath.split(process.cwd())[1]
             });
             return;
           }
 
           fetch(url, {
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify({
-              text: `[eslint] ${message.message} - ([${message.ruleId}](${eslintDocsBaseUrl}/${
-                message.ruleId
-              }.md))`,
+              text: `[eslint] ${message.message} - ([${message.ruleId}](${eslintDocsBaseUrl}/${message.ruleId}.md))`,
               anchor: {
                 line: message.line,
-                lineType: 'ADDED',
-                fileType: 'TO',
-                path: file.filePath.split(process.cwd())[1],
-              },
+                lineType: "ADDED",
+                fileType: "TO",
+                path: file.filePath.split(process.cwd())[1]
+              }
             }),
-            credentials: 'include',
+            credentials: "include",
             headers: {
               Authorization: encodeAuthorization(options),
-              'Content-Type': 'application/json',
-            },
+              "Content-Type": "application/json"
+            }
           }).catch(console.log);
         });
     }
   });
 };
 
-module.exports.run = function(
-  {
-    bitbucketUrl = process.env.BITBUCKET_URL,
-    commentFileLevel = true,
-    commentTopLevel = true,
-    createTask = true,
-    debug = false,
-    warnings = true,
-    lintResultsPath = process.env.LINT_RESULTS_PATH,
-    jobName = process.env.JOB_NAME,
-    password = process.env.BITBUCKET_PASSWORD,
-    project = process.env.BITBUCKET_PROJECT,
-    pullRequestID = process.env.PULL_REQUEST_ID,
-    repository = process.env.BITBUCKET_REPOSITORY,
-    user = process.env.BITBUCKET_USER,
-  } = {}
-) {
+module.exports.run = function({
+  bitbucketUrl = process.env.BITBUCKET_URL,
+  commentFileLevel = true,
+  commentTopLevel = true,
+  createTask = true,
+  debug = false,
+  warnings = true,
+  lintResultsPath = process.env.LINT_RESULTS_PATH,
+  jobName = process.env.JOB_NAME,
+  password = process.env.BITBUCKET_PASSWORD,
+  project = process.env.BITBUCKET_PROJECT,
+  pullRequestID = process.env.PULL_REQUEST_ID,
+  repository = process.env.BITBUCKET_REPOSITORY,
+  user = process.env.BITBUCKET_USER
+} = {}) {
   if (jobName) {
-    repository = repository || jobName.split('/')[0];
+    repository = repository || jobName.split("/")[0];
     if (!/PR-(\d*)/.test(jobName)) {
-      console.log('Job is not a PR, and there is no supplied pullRequestID');
+      console.log("Job is not a PR, and there is no supplied pullRequestID");
       return;
     }
-    pullRequestID = pullRequestID || jobName.split('/')[1].match(/PR-(\d*)/)[1];
+    pullRequestID = pullRequestID || jobName.split("/")[1].match(/PR-(\d*)/)[1];
   }
 
   try {
@@ -148,7 +147,7 @@ module.exports.run = function(
       project,
       pullRequestID,
       repository,
-      user,
+      user
     });
 
     const lintResults = require(path.resolve(process.cwd(), lintResultsPath));
@@ -156,10 +155,20 @@ module.exports.run = function(
 
     if (lintResults && lintResults.length) {
       if (commentTopLevel) {
-        commentTopLevelFn(lintResults, url, bitbucketUrl, { debug, user, password, createTask });
+        commentTopLevelFn(lintResults, url, bitbucketUrl, {
+          debug,
+          user,
+          password,
+          createTask
+        });
       }
       if (commentFileLevel) {
-        commentFileLevelFn(lintResults, url, { debug, user, password, warnings });
+        commentFileLevelFn(lintResults, url, {
+          debug,
+          user,
+          password,
+          warnings
+        });
       }
     }
   } catch (err) {
